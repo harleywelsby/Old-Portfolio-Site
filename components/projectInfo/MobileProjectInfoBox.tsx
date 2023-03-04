504bd4ebea3a24152d310de00502fb9034c6ad29
harleywelsby <harleywelsby9@gmail.com>
2023-03-04 13:07:05 +1300

    Mobile fix for projects page

--- components/projectInfo/MobileProjectInfoBox.tsx
+++ components/projectInfo/MobileProjectInfoBox.tsx
@@ -8,6 +8,8 @@ import {
 import React from "react";
 import Image from "next/image";
 import { ProjectInfoBoxProps } from "./ProjectInfoBoxProps";
+import LinkLogo from "../LinkLogo";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function MobileProjectInfoBox({
   title,
@@ -20,7 +22,14 @@ function MobileProjectInfoBox({
       <InfoBoxContent>
         <FlexColumn>
           <NameText className="mobileProjectNameText">{title}</NameText>
-          <DescriptionText className="mobileDescriptionText">
+          <LinkLogo
+            iconName={faGithub}
+            link={link}
+            size={"2x"}
+            newTab
+            padding
+          />
+          <DescriptionText className="mobileDescriptionText negative1TopMargin">
             {description}
           </DescriptionText>
           <Image
